e17f051e164b8ae50f7d800db881fe3806baa559
al-burlachenko <alexandrburl@gmail.com>
2025-12-02 03:17:23 +0200

    adds functionality

--- script.js
+++ script.js
@@ -1,176 +1,201 @@
-// setup
-const canvas = document.getElementById("canvas1");
-const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+window.addEventListener("load", () => {
+  // setup
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height); // x0 y0 x1 y1
-gradient.addColorStop(0, "white");
-gradient.addColorStop(0.5, "gold");
-gradient.addColorStop(1, "orangered");
+  ctx.strokeStyle = "white";
 
-ctx.fillStyle = gradient;
-ctx.strokeStyle = gradient;
+  class Whale {
+    constructor(effect) {
+      this.effect = effect;
+      this.x = this.effect.width * 0.4;
+      this.y = this.effect.height * 0.5;
+      this.image = document.getElementById("whale3");
+      this.angle = 0;
+      this.va = 0.01;
+      this.curve = this.effect.height * 0.25;
+      this.spriteWidth = 420;
+      this.spriteHeight = 285;
+      this.frameX = 0;
+      this.frameY = Math.floor(Math.random() * 4);
+      this.maxFrame = 38;
+      this.frameTimer = 0;
+      this.frameInterval = 1000 / 60;
+      this.radius = 200;
+    }
 
-class Particle {
-  constructor(effect, index) {
-    this.index = index;
-    this.effect = effect;
-    this.radius = Math.floor(Math.random() * 10 + 1);
+    draw(context) {
+      context.save();
+      context.translate(this.x, this.y);
+      context.rotate(Math.cos(this.angle) * 0.4);
+      context.drawImage(
+        this.image,
+        this.frameX * this.spriteWidth,
+        this.frameY * this.spriteHeight,
+        this.spriteWidth,
+        this.spriteHeight,
+        0 - this.spriteWidth * 0.5,
+        0 - this.spriteHeight * 0.5,
+        this.spriteWidth,
+        this.spriteHeight
+      );
+      context.restore();
+    }
 
-    this.x =
-      this.radius + Math.random() * (this.effect.width - this.radius * 2);
-    this.y =
-      this.radius + Math.random() * (this.effect.height - 2 * this.radius);
-    this.vx = Math.random() * 1 - 0.5;
-    this.vy = Math.random() * 1 - 0.5;
+    update(deltaTime) {
+      this.angle += this.va;
+      this.y = this.effect.height * 0.5 + Math.sin(this.angle) * this.curve;
+      if (this.angle > Math.PI * 2) this.angle = 0;
 
-    this.pushX = 0;
-    this.pushY = 0;
-    this.friction = 0.8;
+      // fps
+      if (this.frameTimer > this.frameInterval) {
+        // sprite animation
+
+        this.frameX < this.maxFrame ? (this.frameX += 1) : (this.frameX = 0);
+        this.frameTimer = 0;
+      } else {
+        this.frameTimer += deltaTime;
+      }
+    }
   }
-  draw(context) {
-    context.beginPath();
-    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    context.fill();
-  }
 
-  update() {
-    if (this.effect.mouse.pressed) {
-      const dx = this.x - this.effect.mouse.x;
-      const dy = this.y - this.effect.mouse.y;
+  class Particle {
+    constructor(effect, index) {
+      this.index = index;
+      this.effect = effect;
+      this.radius = Math.floor(Math.random() * 6 + 1);
+      this.imageSize = this.radius * 8;
+      this.halfImageSize = this.imageSize / 2;
+      this.x =
+        Math.random() * (this.effect.width + this.effect.maxDistance * 2);
+      this.y = Math.random() * this.effect.height;
+      this.vx = -1.5;
+
+      this.pushX = 0;
+      this.pushY = 0;
+      this.friction = 0.99;
+      this.image = document.getElementById("star");
+    }
+    draw(context) {
+      // context.beginPath();
+      // context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+      // context.fill();
+      context.drawImage(
+        this.image,
+        this.x - this.halfImageSize,
+        this.y - this.halfImageSize,
+        this.imageSize,
+        this.imageSize
+      );
+    }
+
+    update() {
+      const dx = this.x - this.effect.whale.x;
+      const dy = this.y - this.effect.whale.y;
       const distance = Math.hypot(dx, dy);
-      const force = this.effect.mouse.radius / distance;
-      if (distance < this.effect.mouse.radius) {
+      const force = this.effect.whale.radius / distance;
+      if (distance < this.effect.whale.radius) {
         const angle = Math.atan2(dy, dx);
         this.pushY += Math.sin(angle) * force;
         this.pushX += Math.cos(angle) * force;
       }
-    }
 
-    this.x += (this.pushX *= this.friction) + this.vx;
-    this.y += (this.pushY *= this.friction) + this.vy;
+      this.x += (this.pushX *= this.friction) + this.vx;
+      this.y += this.pushY *= this.friction;
 
-    if (this.x < this.radius) {
-      this.x = this.radius;
-      this.vx *= -1;
-    }
-    if (this.x > this.effect.width - this.radius) {
-      this.x = this.effect.width - this.radius;
-      this.vx *= -1;
-    }
-    if (this.y < this.radius) {
-      this.y = this.radius;
-      this.vy *= -1;
-    }
-    if (this.y > this.effect.height - this.radius) {
-      this.y = this.effect.height - this.radius;
-      this.vy *= -1;
-    }
-  }
-
-  reset() {
-    this.x =
-      this.radius + Math.random() * (this.effect.width - this.radius * 2);
-    this.y =
-      this.radius + Math.random() * (this.effect.height - this.radius * 2);
-  }
-}
-
-class Effect {
-  constructor(canvas, context) {
-    this.canvas = canvas;
-    this.context = context;
-    this.width = this.canvas.width;
-    this.height = this.canvas.height;
-    this.particles = [];
-    this.numberOfParticles = Math.floor(Math.random() * 200 + 400);
-    this.createParticles();
-
-    this.mouse = {
-      x: 0,
-      y: 0,
-      pressed: false,
-      radius: 200,
-    };
-
-    window.addEventListener("resize", (e) => {
-      this.resize(e.target.window.innerWidth, e.target.window.innerHeight);
-    });
-    window.addEventListener("mousemove", (e) => {
-      if (this.mouse.pressed) {
-        this.mouse.x = e.x;
-        this.mouse.y = e.y;
+      if (this.x < -this.imageSize - this.effect.maxDistance) {
+        this.x = this.effect.width + this.imageSize + this.effect.maxDistance;
       }
-    });
-    window.addEventListener("mousedown", (e) => {
-      this.mouse.pressed = true;
-      this.mouse.x = e.x;
-      this.mouse.y = e.y;
-    });
-    window.addEventListener("mouseup", (e) => {
-      this.mouse.pressed = false;
-    });
-  }
+    }
 
-  createParticles() {
-    for (let i = 0; i < this.numberOfParticles; i += 1) {
-      this.particles.push(new Particle(this, i));
+    reset() {
+      this.x =
+        Math.random() * (this.effect.width + this.effect.maxDistance * 2);
+      this.y = Math.random() * this.effect.height;
     }
   }
 
-  handleParticles(context) {
-    this.connectParticles(context);
-    this.particles.forEach((particle) => {
-      particle.draw(context);
-      particle.update();
-    });
-  }
+  class Effect {
+    constructor(canvas, context) {
+      this.canvas = canvas;
+      this.context = context;
+      this.width = this.canvas.width;
+      this.height = this.canvas.height;
+      this.particles = [];
+      this.numberOfParticles = 300;
+      this.maxDistance = 110;
+      this.createParticles();
+      this.whale = new Whale(this);
 
-  connectParticles(context) {
-    const maxDistance = 60;
-    for (let a = 0; a < this.particles.length; a += 1) {
-      for (let b = a; b < this.particles.length; b += 1) {
-        const dx = this.particles[a].x - this.particles[b].x;
-        const dy = this.particles[a].y - this.particles[b].y;
-        // const distance = Math.sqrt(dx * dx + dy * dy);
-        const distance = Math.hypot(dx, dy); // same
-        if (distance < maxDistance) {
-          context.save();
-          const opacity = 1 - distance / maxDistance;
-          context.globalAlpha = opacity;
-          context.beginPath();
-          context.moveTo(this.particles[a].x, this.particles[a].y);
-          context.lineTo(this.particles[b].x, this.particles[b].y);
-          context.stroke();
-          context.restore();
+      window.addEventListener("resize", (e) => {
+        this.resize(e.target.window.innerWidth, e.target.window.innerHeight);
+      });
+    }
+
+    createParticles() {
+      for (let i = 0; i < this.numberOfParticles; i += 1) {
+        this.particles.push(new Particle(this, i));
+      }
+    }
+
+    handleParticles(context, deltaTime) {
+      this.whale.draw(context);
+      this.whale.update(deltaTime);
+      this.connectParticles(context);
+      this.particles.forEach((particle) => {
+        particle.draw(context);
+        particle.update();
+      });
+    }
+
+    connectParticles(context) {
+      for (let a = 0; a < this.particles.length; a += 1) {
+        for (let b = a; b < this.particles.length; b += 1) {
+          const dx = this.particles[a].x - this.particles[b].x;
+          const dy = this.particles[a].y - this.particles[b].y;
+          // const distance = Math.sqrt(dx * dx + dy * dy);
+          const distance = Math.hypot(dx, dy); // same
+          if (distance < this.maxDistance) {
+            context.save();
+            const opacity = 1 - distance / this.maxDistance;
+            context.globalAlpha = opacity;
+            context.beginPath();
+            context.moveTo(this.particles[a].x, this.particles[a].y);
+            context.lineTo(this.particles[b].x, this.particles[b].y);
+            context.stroke();
+            context.restore();
+          }
         }
       }
     }
+    //
+    resize(width, height) {
+      this.canvas.width = width;
+      this.canvas.height = height;
+      this.width = width;
+      this.height = height;
+      // resize event resets context to its default so:
+      this.whale.x = this.width * 0.4;
+      this.whale.y = this.height * 0.5;
+
+      this.whale.curve = this.height * 0.2;
+
+      this.context.strokeStyle = "white";
+      this.particles.forEach((particle) => particle.reset());
+    }
   }
-  //
-  resize(width, height) {
-    this.canvas.width = width;
-    this.canvas.height = height;
-    this.width = width;
-    this.height = height;
-    // resize event resets context to its default so:
-    const gradient = ctx.createLinearGradient(0, 0, width, height); // x0 y0 x1 y1
-    gradient.addColorStop(0, "white");
-    gradient.addColorStop(0.5, "gold");
-    gradient.addColorStop(1, "orangered");
-    this.context.fillStyle = gradient;
-    this.context.strokeStyle = "white";
 
-    this.particles.forEach((particle) => particle.reset());
+  const effect = new Effect(canvas, ctx);
+
+  let lastTime = 0;
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    effect.handleParticles(ctx, deltaTime);
+    requestAnimationFrame(animate);
   }
-}
-
-const effect = new Effect(canvas, ctx);
-
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  effect.handleParticles(ctx);
-  requestAnimationFrame(animate);
-}
-animate();
+  animate(0);
+});
